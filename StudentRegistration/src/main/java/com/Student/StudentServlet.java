36ee547ad33ac74a9e686850f8220efa5d3f8e1f
AvadhutJoshi012 <avadhutjoshi012@gmail.com>
2025-12-08 17:35:48 +0530

    Added Student Registration Example

---
+++ StudentRegistration/src/main/java/com/Student/StudentServlet.java
@@ -0,0 +1,20 @@
+package com.Student;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class StudentServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/heml");
+		
+	}
+
+}
